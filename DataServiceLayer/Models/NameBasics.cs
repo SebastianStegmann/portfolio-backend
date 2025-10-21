2ba59384f3d4bde0e08ba4577629ecee7e9eeb87
SofNyb <snjwindeloev@gmail.com>
2025-10-21 13:08:47 +0200

    Added NameBasics and attributes

---
+++ DataServiceLayer/Models/NameBasics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataServiceLayer.Models;
+
+public class NameBasics
+{
+    public string Nconst { get; set; } = string.Empty;
+    public string? Name { get; set; }
+    public string? BirthYear { get; set; }
+    public string? DeathYear { get; set; }
+    public string? NameRating { get; set; }
+}
