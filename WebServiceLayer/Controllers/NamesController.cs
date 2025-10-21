3765d27a40e90776f00b2e8a2ba84580ec292c7e
SofNyb <snjwindeloev@gmail.com>
2025-10-21 13:09:03 +0200

    Added NameController and logic

---
+++ WebServiceLayer/Controllers/NamesController.cs
@@ -0,0 +1,30 @@
+﻿using DataServiceLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServiceLayer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NamesController : Controller
+{
+    private readonly DataService _dataService;
+
+    public NamesController(DataService dataService)
+    {
+        _dataService = dataService;
+    }
+
+    [HttpGet]
+    public IActionResult GetNames()
+    {
+        return Ok(_dataService.GetTitles());
+    }
+
+    [HttpGet("{nconst}")]
+    public IActionResult GetNames(string nconst)
+    {
+        var title = _dataService.GetName(nconst);
+        if (title == null) return NotFound();
+        return Ok(title);
+    }
+}
