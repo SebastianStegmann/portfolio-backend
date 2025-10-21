ae1c16a6297e0721c36209105ac90ed50fa7e9b5
SofNyb <snjwindeloev@gmail.com>
2025-10-21 20:23:06 +0200

    moved NameBasics to a folder

--- DataServiceLayer/Models/NameBasics/NameBasics.cs
+++ DataServiceLayer/Models/NameBasics/NameBasics.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DataServiceLayer.Models;
+namespace DataServiceLayer.Models.NameBasics;
 
 public class NameBasics
 {
-    public string Nconst { get; set; } = string.Empty;
+    public string Nconst { get; set; } = string.Empty; // Can never be null, starts as ""
     public string? Name { get; set; }
     public string? BirthYear { get; set; }
     public string? DeathYear { get; set; }
