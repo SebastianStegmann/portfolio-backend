6d8f3684399e596eaff5eae6ed323828334898a1
Sebastian Stegmann <sebastianbille@protonmail.com>
2025-10-23 15:27:33 +0200

    Added JWT stuff to program.cs

--- WebServiceLayer/Program.cs
+++ WebServiceLayer/Program.cs
@@ -1,8 +1,28 @@
 using DataServiceLayer;
 using Microsoft.EntityFrameworkCore;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add JWT
+var key = Encoding.ASCII.GetBytes("your-32-char-secret-key-here"); // Use secure key in prod
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+  .AddJwtBearer(options =>
+      {
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+      ValidateIssuerSigningKey = true,
+      IssuerSigningKey = new SymmetricSecurityKey(key),
+      ValidateIssuer = false,
+      ValidateAudience = false
+      };
+      });
+
+builder.Services.AddAuthorization();
+// End JWT
 
 builder.Configuration.AddJsonFile("config.json");
 
@@ -13,53 +33,23 @@ builder.Services.AddDbContext<ImdbContext>(options =>
 
 Console.WriteLine(connectionString);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-// builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-
-// builder.Services.AddDbContext<ImdbContext>(options =>
-//     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<TitleDataService>();
 builder.Services.AddScoped<NameDataService>();
 builder.Services.AddScoped<PersonDataService>();
 
+//jwt
+builder.Services.AddAuthorization();
+//end jwt
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.MapOpenApi();
-// }
-
+// JWT
+app.UseAuthentication();
+app.UseAuthorization();
+// end JWT
+//
 app.MapControllers();
 
-// app.UseHttpsRedirection();
-
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
-//
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast =  Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast");
-
 app.Run();
-
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
