8ef4f18facb6d6f1c6fe63dbd6750cbd6678aef8
SofNyb <snjwindeloev@gmail.com>
2025-10-24 12:47:28 +0200

    added TitleModel

--- WebServiceLayer/Models/TitleModel.cs
+++ WebServiceLayer/Models/TitleModel.cs
@@ -2,5 +2,17 @@
 {
     public class TitleModel
     {
+        public string? URL { get; set; }
+        public string? TitleType { get; set; }
+        public string PrimaryTitle { get; set; } = string.Empty;
+        public string OriginalTitle { get; set; } = string.Empty;
+        public bool? IsAdult { get; set; }
+        public string? ReleaseDate { get; set; }
+        public string? EndYear { get; set; }
+        public short? TotalSeasons { get; set; }
+        public string? Plot { get; set; }
+        public string? Poster { get; set; }
+        public string? Country { get; set; }
+        public int? RuntimeMinutes { get; set; }
     }
 }
