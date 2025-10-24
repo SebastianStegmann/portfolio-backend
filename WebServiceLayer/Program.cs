b80a8d56d1cbc5ff445f6f6802c2beaee3b24c8a
SofNyb <snjwindeloev@gmail.com>
2025-10-24 10:40:47 +0200

    Cleanup on Program.cs

--- WebServiceLayer/Program.cs
+++ WebServiceLayer/Program.cs
@@ -13,13 +13,7 @@ builder.Services.AddDbContext<ImdbContext>(options =>
 
 Console.WriteLine(connectionString);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-// builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-
-// builder.Services.AddDbContext<ImdbContext>(options =>
-//     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<TitleDataService>();
 builder.Services.AddScoped<NameDataService>();
@@ -28,38 +22,8 @@ builder.Services.AddScoped<PersonDataService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.MapOpenApi();
-// }
-
 app.MapControllers();
-
-// app.UseHttpsRedirection();
-
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
-//
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast =  Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast");
 
 app.Run();
 
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
+
