0ecd0063c7e06c9ac1c76a296b49d464beb29bb1
SofNyb <snjwindeloev@gmail.com>
2025-10-24 11:08:54 +0200

    Add Mapser

--- WebServiceLayer/Program.cs
+++ WebServiceLayer/Program.cs
@@ -1,10 +1,13 @@
 using DataServiceLayer;
+using Mapster;
 using Microsoft.EntityFrameworkCore;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("config.json");
+
+// Add services to the container
 
 var connectionString = builder.Configuration.GetSection("ConnectionString").Value;
 
@@ -13,14 +16,18 @@ builder.Services.AddDbContext<ImdbContext>(options =>
 
 Console.WriteLine(connectionString);
 
+
+builder.Services.AddMapster();
+
 builder.Services.AddControllers();
 
 builder.Services.AddScoped<TitleDataService>();
 builder.Services.AddScoped<NameDataService>();
 builder.Services.AddScoped<PersonDataService>();
 
-
 var app = builder.Build();
+
+// Configure the HTTP request pipeline
 
 app.MapControllers();
 
