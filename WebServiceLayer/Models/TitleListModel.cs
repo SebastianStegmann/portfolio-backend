26079e2e14bf2e551dd608dd8fd19a4ae67b2637
SofNyb <snjwindeloev@gmail.com>
2025-10-27 12:55:22 +0100

    add TItleListModel

---
+++ WebServiceLayer/Models/TitleListModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebServiceLayer.Models
+{
+    public class TitleListModel
+    {
+        public string? URL { get; set; }
+        public string? TitleType { get; set; }
+        public string PrimaryTitle { get; set; } = string.Empty;
+        public string? ReleaseDate { get; set; }
+        public short? TotalSeasons { get; set; }
+        public string? Poster { get; set; }
+        public string? AllActorURL { get; set; }
+    }
+}
