7baf364c21968706128155d01562edeefc407f45
SofNyb <snjwindeloev@gmail.com>
2025-10-27 14:48:45 +0100

    added Category, job and characters for them to shown in endpoint around allcast

--- WebServiceLayer/Models/NameListModel.cs
+++ WebServiceLayer/Models/NameListModel.cs
@@ -4,6 +4,10 @@
     {
         public string? URL { get; set; }
         public string? Name { get; set; }
+        public string? Category { get; set; }
+        public string? Characters { get; set; }
+        public string? Job { get; set; }
         public string? KnownForURL { get; set; }
+
     }
 }
