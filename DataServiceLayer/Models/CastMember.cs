615583612884ccb803ab59a9e0870e29d8d32538
SofNyb <snjwindeloev@gmail.com>
2025-10-27 14:50:52 +0100

    added OTD castmember to connect titleprincipals and namebasics

---
+++ DataServiceLayer/Models/CastMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataServiceLayer.Models
+{
+    public class CastMember
+    {
+        public string Nconst { get; set; } = string.Empty;
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public string? Job { get; set; }
+        public string? Characters { get; set; }
+        public short? Ordering { get; set; }
+    }
+}
