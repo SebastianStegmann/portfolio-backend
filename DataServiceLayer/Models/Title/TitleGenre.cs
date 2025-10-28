51ba5ac98fdf8b01c0dadfebe16fc54a373b38aa
SofNyb <snjwindeloev@gmail.com>
2025-10-28 17:00:42 +0100

    adding navigation to genre table to get the genrename

--- DataServiceLayer/Models/Title/TitleGenre.cs
+++ DataServiceLayer/Models/Title/TitleGenre.cs
@@ -4,4 +4,7 @@ public class TitleGenre
 {
     public string Tconst { get; set; } = string.Empty;
     public short GenreId { get; set; }
+
+    // Navigation to Genre
+    public Genre? Genre { get; set; }
 }
