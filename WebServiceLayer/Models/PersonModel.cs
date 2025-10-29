9f71eb529378423592a800b7eada25284fb00205
SofNyb <snjwindeloev@gmail.com>
2025-10-29 16:23:25 +0100

    created the personmodel and personlistmodel

---
+++ WebServiceLayer/Models/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebServiceLayer.Models
+{
+    public class PersonModel
+    {
+        public string? URL { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime? Birthday { get; set; }
+        public string? Location { get; set; }
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public DateTime? LastLogin { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
