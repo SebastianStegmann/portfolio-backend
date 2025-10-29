e73fcfc2547970a0ca5ee472c135b4c603913a89
SofNyb <snjwindeloev@gmail.com>
2025-10-29 09:41:27 +0100

    add rating dto

---
+++ WebServiceLayer/Models/DTO/RatingDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace WebServiceLayer.Models.DTO
+{
+    public class RatingDTO
+    {
+        public string Rating { get; set; } = string.Empty;
+        public string Votes { get; set; } = string.Empty;
+    }
+}
