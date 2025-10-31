aa22bf96509e58fec8f31b353b8e308c75d52236
SofNyb <snjwindeloev@gmail.com>
2025-10-31 12:23:08 +0100

    remove unused methods

--- DataServiceLayer/NameDataService.cs
+++ DataServiceLayer/NameDataService.cs
@@ -52,96 +52,11 @@ public class NameDataService : BaseDataService
       .ToList();
   }
 
-  // All professions for a person
-  public List<Profession> GetNameProfessions(string nconst)
-  {
-    return _context.NameProfessions
-      .Where(np => np.Nconst == nconst)
-      .Join(_context.Professions,
-          np => np.ProfessionId,
-          p => p.Id,
-          (np, p) => p)
-      .ToList();
-  }
-
-    // All people with a specific profession
-    public List<NameBasics> GetNamesByProfession(int professionId)
-    {
-        return _context.NameProfessions
-          .Where(np => np.ProfessionId == professionId)
-          .Join(_context.NameBasics,
-              np => np.Nconst,
-              nb => nb.Nconst,
-              (np, nb) => nb)
-          .ToList();
-    }
-
-  // All people known for a specific movie
-  public List<NameBasics> GetNamesKnownForTitle(string tconst)
-  {
-    return _context.KnownFors
-      .Where(kf => kf.Tconst == tconst)
-      .Join(_context.NameBasics,
-          kf => kf.Nconst,
-          nb => nb.Nconst,
-          (kf, nb) => nb)
-      .ToList();
-  }
-
   // All professions
   public List<Profession> GetAllProfessions()
   {
     return _context.Professions.ToList();
   }
-
-    // All roles for a specific person in a specific title
-    public List<Role> GetNameRolesInTitle(string nconst, string tconst)
-    {
-        return _context.NameTitleRoles
-          .Where(ntr => ntr.Nconst == nconst && ntr.Tconst == tconst)
-          .Join(_context.Roles,
-              ntr => ntr.RoleId,
-              r => r.RoleId,
-              (ntr, r) => r)
-          .ToList();
-    }
-
-    // All people with a specific role in a title
-    public List<NameBasics> GetNamesByRoleInTitle(string tconst, int roleId)
-    {
-        return _context.NameTitleRoles
-          .Where(ntr => ntr.Tconst == tconst && ntr.RoleId == roleId)
-          .Join(_context.NameBasics,
-              ntr => ntr.Nconst,
-              nb => nb.Nconst,
-              (ntr, nb) => nb)
-          .ToList();
-    }
-
-    // All roles for a person across all titles
-    public List<Role> GetAllRolesForName(string nconst)
-    {
-        return _context.NameTitleRoles
-          .Where(ntr => ntr.Nconst == nconst)
-          .Join(_context.Roles,
-              ntr => ntr.RoleId,
-              r => r.RoleId,
-              (ntr, r) => r)
-          .Distinct()
-          .ToList();
-    }
-
-    // All titles where a person had a specific role
-    public List<TitleBasics> GetTitlesByNameAndRole(string nconst, int roleId)
-    {
-        return _context.NameTitleRoles
-          .Where(ntr => ntr.Nconst == nconst && ntr.RoleId == roleId)
-          .Join(_context.TitleBasics,
-              ntr => ntr.Tconst,
-              tb => tb.Tconst,
-              (ntr, tb) => tb)
-          .ToList();
-    }
 
 
     // Helpers
--- WebServiceLayer/Controllers/NamesController.cs
+++ WebServiceLayer/Controllers/NamesController.cs
@@ -62,68 +62,6 @@ public class NamesController : BaseController<NameDataService>
         return Ok(titleListModels);
     }
 
-    // Getting the professions for one actor - GET: api/names/{nconst}/professions
-    [HttpGet("{nconst}/professions")]
-    public IActionResult GetNameProfessions(string nconst)
-    {
-        var professions = _dataService.GetNameProfessions(nconst);
-        if (professions == null || professions.Count == 0) return NotFound();
-        return Ok(professions);
-    }
-
-    // Getting all names with a specific profession - GET: api/names/byprofession/{professionId}
-    [HttpGet("byprofession/{professionId}")]
-    public IActionResult GetNamesByProfession(int professionId)
-    {
-        var names = _dataService.GetNamesByProfession(professionId);
-        if (names == null || names.Count == 0) return NotFound();
-        return Ok(names);
-    }
-
-    // Getting all names known for a specific title - GET: api/names/knownfortitle/{tconst}
-    [HttpGet("knownfortitle/{tconst}")]
-    public IActionResult GetNamesKnownForTitle(string tconst)
-    {
-        var names = _dataService.GetNamesKnownForTitle(tconst);
-        if (names == null || names.Count == 0) return NotFound();
-        return Ok(names);
-    }
-    // Getting the roles of a name in a specific title - GET: api/names/{nconst}/roles/title/{tconst}
-    [HttpGet("{nconst}/roles/title/{tconst}")]
-    public IActionResult GetNameRolesInTitle(string nconst, string tconst)
-    {
-        var roles = _dataService.GetNameRolesInTitle(nconst, tconst);
-        if (roles == null || roles.Count == 0) return NotFound();
-        return Ok(roles);
-    }
-
-    // Getting all roles for a name - GET: api/names/{nconst}/roles
-    [HttpGet("{nconst}/roles")]
-    public IActionResult GetAllRolesForName(string nconst)
-    {
-        var roles = _dataService.GetAllRolesForName(nconst);
-        if (roles == null || roles.Count == 0) return NotFound();
-        return Ok(roles);
-    }
-
-    // Getting all titles for a name with a specific role - GET: api/names/{nconst}/titles/role/{roleId}
-    [HttpGet("{nconst}/titles/role/{roleId}")]
-    public IActionResult GetTitlesByNameAndRole(string nconst, int roleId)
-    {
-        var titles = _dataService.GetTitlesByNameAndRole(nconst, roleId);
-        if (titles == null || titles.Count == 0) return NotFound();
-        return Ok(titles);
-    }
-
-    // Getting all names with a specific role in a title - GET: api/names/title/{tconst}/role/{roleId}
-    [HttpGet("title/{tconst}/role/{roleId}")]
-    public IActionResult GetNamesByRoleInTitle(string tconst, int roleId)
-    {
-        var names = _dataService.GetNamesByRoleInTitle(tconst, roleId);
-        if (names == null || names.Count == 0) return NotFound();
-        return Ok(names);
-    }
-
     //object-object mapping
 
     // Information shown when listing all the names
