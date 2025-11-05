87e2c1ce8670ff6074c0ce232f2ffb7c8d4c91e4
Sebastian Stegmann <sebastianbille@protonmail.com>
2025-11-05 15:42:10 +0100

    bestmatch update model

--- DataServiceLayer/Models/Functions/BestMatchResult.cs
+++ DataServiceLayer/Models/Functions/BestMatchResult.cs
@@ -2,7 +2,7 @@ namespace DataServiceLayer.Models.Functions;
 
 public class BestMatchResult
 {
-    public string Tconst { get; set; } = null!;
-    public string Primarytitle { get; set; } = null!;
+    public required string Tconst { get; set; }
+    public required string Primarytitle { get; set; }
     public int Match_Count { get; set; }
 }
